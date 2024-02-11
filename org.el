f59f8b0d61d7234cdae83d368389a4b2e5016f67
Steffen Haug <stehau@pm.me>
2024-02-11 13:22:55 +0100

    Org Polymode

--- org.el
+++ org.el
@@ -9,9 +9,22 @@
     'org-babel-variable-assignments:julia-vterm)
   (setenv "JULIA_PROJECT" "@."))
 
+(use-package polymode
+  :ensure t)
+
+(use-package poly-org
+  :ensure t
+  :after polymode)
+
 (setq org-format-latex-options
       (plist-put org-format-latex-options :scale 1.5))
 
 (setq org-confirm-babel-evaluate nil)
 
 (setq org-image-actual-width 500)
+
+(setq org-src-window-setup 'split-window-below)
+(setq org-src-tab-acts-natively t)
+
+
+(keymap-set poly-org-mode-map "C-c C-c" 'org-ctrl-c-ctrl-c)
